a0c2052b61743024b7d465d800a94d33bb3050e2
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-11-25 20:31:58 +0300

    Добавил кастомные слайдер

--- TellMeAMovie/Pods/Target Support Files/Pods-TellMeAMovie/Pods-TellMeAMovie-frameworks.sh
+++ TellMeAMovie/Pods/Target Support Files/Pods-TellMeAMovie/Pods-TellMeAMovie-frameworks.sh
@@ -86,8 +86,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TMDBSwift/TMDBSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/YSRangeSlider/YSRangeSlider.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TMDBSwift/TMDBSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/YSRangeSlider/YSRangeSlider.framework"
 fi

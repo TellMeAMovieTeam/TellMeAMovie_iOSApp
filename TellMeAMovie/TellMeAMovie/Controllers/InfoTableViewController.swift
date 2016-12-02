97935df2df79cfbe53be760137c67c837624337a
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-12-02 20:59:55 +0300

    Сделал отправку сообщений (дз 4)

--- TellMeAMovie/TellMeAMovie/Controllers/InfoTableViewController.swift
+++ TellMeAMovie/TellMeAMovie/Controllers/InfoTableViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class InfoTableViewController: UITableViewController {
+class InfoTableViewController: UITableViewController, MFMailComposeViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,34 @@ class InfoTableViewController: UITableViewController {
 
     @IBAction func writeLetter(_ sender: UIButton) {
         
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["ilyagutnikov93@gmail.com"])
+        mailComposerVC.setSubject("TellMeAMovie in-app e-mail")
+        mailComposerVC.setMessageBody("Сообщение отправлено из приложения TellMeAMovie", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Невозможно отправить сообщение", message: "Ваше устройство не может отправить e-mail. Пожалуйста, измените конфигурацию и попробуйте снова.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate
+    
+    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
+        controller.dismiss(animated: true, completion: nil)
         
     }
 
